3bedd5af625bc9b32524f3dfd92122a06ec5a574
HaMinhKhoa <80310599+HaMinhKhoa@users.noreply.github.com>
2021-04-29 23:39:43 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,63 @@
+<?php
+    include "includes/header.php";
+?>
+
+<div class="logo">
+<img src="https://s3.amazonaws.com/freebiesupply/large/2x/toronto-raptors-logo-transparent.png" width=100px alt="logo">
+</div>
+<div class="main-nav">
+<nav>
+<ul>
+<li><a href="#">Home</a></li>
+<li><a href="#">About Me</a></li>
+<li><a href="#">Hobbies</a></li>
+<li><a href="resume.html">Resume</a></li>
+</ul>
+</nav>
+</div>
+
+<div id="banner">
+<div class="quote">
+<h1>Don't cry because it's over. Smile because it happened. ~ Dr.Seuss</h1>
+<h4>I am Khoa</h4>
+<p>From VietNam with love</p>
+</div>
+</div> <!-- end of #Banner -->
+<div id="main-col">
+<div class="col-3">
+<h2>About Me</h2>
+<p>I live in SaiGon,VietNam. I'm an ambivert person who down to talk to shy people and insecure when surround by lot of confident strangers. Their energy unmatch my anxiety :)</p>
+</div>
+
+<div class="col-3">
+<h2>Hobbies</h2>
+<p> I like playing online game,watching movies with family and friends but TV series alone.I'm a aquarium hobbyist, meaning that I like aquascaping, shrimp breeding,etc. I also like basketball, swimming.</p>
+<img src="http://game4v.com/g4v-content/uploads/2020/12/One-Piece-1000-2-game4v.jpg" alt="">
+<img src="https://genk.mediacdn.vn/thumb_w/660/139269124445442048/2021/1/5/1-1609821748300935640933.jpeg" alt="">
+<img src="https://i-english.vnecdn.net/2020/09/03/104906663154968998520385340851-5667-7416-1599126968_680x0.jpg" alt="">
+<img src="https://gameworld.vn/wp-content/uploads/2020/06/csgo-visibility-update-appears-to-reveal-opponents-through-walls.jpg" alt="">
+<img src="https://coolwallpapers.me/picsup/468061-aquarium-wallpaper-free-desktop-wallpapers.jpg" alt="">
+</div>
+<div class="col-3">
+<h2>My Resume</h2>
+<p>You can find my resume in the following site! It's pretty suck.</p>
+</div>
+</div> <!-- end of #Main-col -->
+<div class="info">
+<div class="col-2">
+<h2>Some Information</h2>
+<p> Fun fact about me is that when I was a kid I once felt down the stair and later had 3 stitches near my left eye. </p>
+<ul>
+<li>( ͡° ͜ʖ ͡°)</li>
+<li>(˵ ͡o ͜ʖ ͡o˵)</li>
+<li>(͠≖ ͜ʖ͠≖)</li>
+<li>( ͡°( ͡° ͜ʖ( ͡° ͜ʖ ͡°)ʖ ͡°) ͡°)</li>
+</ul>
+</div>
+<div class="col-2">
+</div>
+</div>
+
+<?php
+    include "includes/footer.php";
+?>
---
+++ resume.php
@@ -0,0 +1,39 @@
+<?php
+  include "./include/header.php"
+?>
+
+<div class="header">
+<img class="logo" src="https://fanart.tv/fanart/movies/4935/movieposter/howls-moving-castle-54c29ec22518e.jpg" alt="Khoa's Photo" width="100">
+<h1><a href="https://www.facebook.com/minhkhoa.ha.5/"><span class="bold-name">Khoa</span> Minh Ha</a></h1>
+<h3>Address: Dong Den St QTanBinh HCMC</h3>
+<h3>Tel +84 793510183</h3>
+<h3>Email: 1959011@itec.hcmus.edu.vn</h3>
+<hr>
+</div> <!-- end of Header -->
+<div class="section">
+<h2>Education</h2>
+<h4>Nguyen Thuong Hien Highschool, HCMC (2016-2019)</h4>
+<h4>University of Science - VNUHCM, HCMC (2019)</h4>
+</div>
+<div class="section">
+<h2>Experience</h2>
+<h4>Student of ITEC and HCMUS</h4>
+<ul>
+<li>Study information technology</li>
+<li>Year 2019-2023</li>
+<li>Finished first year of major</li>
+</ul>
+</div>
+<div class="section">
+<h2>Skills</h2>
+<ul>
+<li>Web Design research</li>
+<li>Advance Database research</li>
+</ul>
+</div>
+
+<a href="index1.html">Go back</a>
+
+<?php
+  include "./include/footer.php"
+?>
